7b95c4bb85c045c69aa90a19053cd5743c0530e3
Andrew Thomas McNulty <atmcnulty@hotmail.com>
2020-10-06 20:36:58 -0400

    added coments to present

--- Divide.java
+++ Divide.java
@@ -1,24 +1,27 @@
 
-public class Divide {
+//public class Divide {
 
 	
 	//Code to make first green line test pass
-	
+	/**
 	public Divide(){
 		
 	}
-	
+	**/
 	//code to make second red line test pass
-	
+	/**
 	public int divide(int x, int y){
 		//Code to make third red line test pass
 		
 		//code to make fourth test pass
+		/**
 		if (y == 0){
 			throw new IllegalArgumentException("Cannot divide by zero");
 		}
+		**/
+	/**
 		return x / y;
 	}
-	
+	*/
 	
 }
--- TestDivide.java
+++ TestDivide.java
@@ -7,22 +7,38 @@ public class TestDivide {
 
 	
 	//First failing red line test
-	@Test
+	@Test //Declares that the function is a Junit test
 	public void assertDivideClassExists(){
-		Divide d = new Divide();
+		Divide d = new Divide(); //Creates a divide object and test if the reference exists
 		Assert.assertNotNull(d);
 		
 	}
 
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/**
 	//Second failing red line test
 	@Test 
 	public void assertDivideMethodAcceptsParameters(){
 		Divide d = new Divide();
 		
-		Assert.assertTrue(d.divide(1,1) == 1);
+		Assert.assertTrue(d.divide(1,1) == 1); //tests that the divide function exists and accepts parameters
 	}
+	**/
 	
+	
+	
+	
+	
+	/**
 	//Third Failing red line test
 	@Test
 	public void assertDivideCanDivideTwoPositiveNumbers(){
@@ -30,11 +46,13 @@ public class TestDivide {
 		
 		
 		
-		Assert.assertEquals(d.divide(9, 3), 3);
+		Assert.assertEquals(d.divide(9, 3), 3); //tests that dividing 9 by 3 returns 3
 	}
+	**/
 	
+	/**
 	//fourth failing red line test
-	@Test(expected = IllegalArgumentException.class)
+	@Test(expected = IllegalArgumentException.class)   //test is expecting an error
 	public void assertDivideByZeroError(){
 		Divide d = new Divide();
 		d.divide(1, 0);
@@ -42,34 +60,38 @@ public class TestDivide {
 		
 		
 	}
-	
+	**/
+	/**
 	//fifth passing red line test
 	@Test
 	public void assertDivideWorksForOneNegativeNumber(){
 		Divide d = new Divide();
 		
-		Assert.assertEquals(d.divide(-16, 4), -4 );
+		Assert.assertEquals(d.divide(-16, 4), -4 ); //test that dividing a negative by positive returns negative
 		
 		
 	}
-	
+	**/
+	/**
 	//sixth passing red line test
 	@Test
 	public void assertDivideWorksForTwoNegativeNumbers(){
 		Divide d = new Divide();
 		
-		Assert.assertEquals(d.divide(-16, -4), 4 );
+		Assert.assertEquals(d.divide(-16, -4), 4 ); //test that dividing a  negative by negative  returns positive
 		
 		
 	}
-	
+	**/
+	/**
 	//seventh passing red line test 
 	@Test
 	public void assertDivideIntoZero(){
 		Divide d = new Divide();
-		Assert.assertEquals(d.divide(0, 1000000), 0);
+		Assert.assertEquals(d.divide(0, 1000000), 0); //test that dividing from 0 returns 0
 	}
-	
+	**/
+
 	
 	
 	
